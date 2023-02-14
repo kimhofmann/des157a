4e1b566a1ec2a627892df8359a1d4130f5db5449
kimberly_hofmann <kchofmann@ucdavis.edu>
2023-02-14 13:31:34 -0800

    i give up :<

--- studio1/script.js
+++ studio1/script.js
@@ -1,14 +1,13 @@
 ( function(){
     'use strict';
     console.log('reading Javascript');
-    const madl= document.getElementById("madlib")
-    const form = document.querySelector('#myform');
-    let formValue;
+    const madl= document.getElementById("madlib");
+    const myForm = document.querySelector('#myform');
 
-
-    form.addEventListener("submit", function(event){
+    myForm.addEventListener('submit', function(event){
         event.preventDefault();
 
+        console.log(`SUBMIT FUNCTION`);
         const bname = document.getElementById('bname').value;
         const aadj = document.getElementById('aadj').value;
         const cname = document.getElementById('cname').value;
@@ -17,17 +16,11 @@
         const excl = document.getElementById('excl').value;
         const fname = document.getElementById('fname').value;
 
-        let MyText; //defined below
+        
+        const text = `This is REPLACED TEXT`;
+        
+        madl.innerHTML = text;
 
-        const myText = ` Dear ${bname}, <br>
-        <br>
-        I hope this email does not find you well. In all honesty, my time working for you has been ${aadj} to say the least. I literally cannot find a single fuck to give any longer, and will be pleased to leave my position at ${cname}. Over the past six years, I have never been more ${hadj} to know that I will no longer have to listen to your incessant whining while you sit around making three times my salary.  I will finally be able to pursue my dream of becoming a ${djob}, a dream driven by my burning desire to get away from you. <br>
-        <br>
-        <br>
-        All the worst and ${excl},<br>
-        ${fname}`;
-
-        madl.innerHTML = myText;
     });
 
     document.querySelector('.open').addEventListener('click', function(event){
@@ -36,11 +29,19 @@
     });
 
     document.addEventListener('keydown', function(event){
-        if (event.key === 'escape') {
+        if (event.key === 'Escape') {
             document.getElementById('overlay').className = 'hidden';
         }
     });
 
 
 })();
+
+/*<p> Dear ${bname}, <br>
+<br>
+I hope this email does not find you well. In all honesty, my time working for you has been ${aadj} to say the least. I literally cannot find a single fuck to give any longer, and will be pleased to leave my position at ${cname}. Over the past six years, I have never been more ${hadj} to know that I will no longer have to listen to your incessant whining while you sit around making three times my salary.  I will finally be able to pursue my dream of becoming a ${djob}, a dream driven by my burning desire to get away from you. <br>
+<br>
+<br>
+All the worst and ${excl},<br>
+${fname} </p>*/
 
