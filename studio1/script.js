76193fcaf41d50e9f7687a2ef801e9184e5b26f3
kimberly_hofmann <kchofmann@ucdavis.edu>
2023-02-14 13:31:46 -0800

    plz

--- studio1/script.js
+++ studio1/script.js
@@ -7,7 +7,7 @@
     myForm.addEventListener('submit', function(event){
         event.preventDefault();
 
-        console.log(`SUBMIT FUNCTION`);
+      
         const bname = document.getElementById('bname').value;
         const aadj = document.getElementById('aadj').value;
         const cname = document.getElementById('cname').value;
@@ -17,7 +17,13 @@
         const fname = document.getElementById('fname').value;
 
         
-        const text = `This is REPLACED TEXT`;
+        const text = `*<p> Dear ${bname}, <br>
+        <br>
+        I hope this email does not find you well. In all honesty, my time working for you has been ${aadj} to say the least. I literally cannot find a single fuck to give any longer, and will be pleased to leave my position at ${cname}. Over the past six years, I have never been more ${hadj} to know that I will no longer have to listen to your incessant whining while you sit around making three times my salary.  I will finally be able to pursue my dream of becoming a ${djob}, a dream driven by my burning desire to get away from you. <br>
+        <br>
+        <br>
+        All the worst and ${excl},<br>
+        ${fname} </p>`;
         
         madl.innerHTML = text;
 
