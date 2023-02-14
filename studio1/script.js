a255df79e898fb06dc2e1fb7c30fb5584df83918
kimberly_hofmann <kchofmann@ucdavis.edu>
2023-02-14 14:38:09 -0800

    THANK YOU DINESH :>

--- studio1/script.js
+++ studio1/script.js
@@ -17,7 +17,7 @@
         const fname = document.getElementById('fname').value;
 
         
-        const text = `*<p> Dear ${bname}, <br>
+        const text = `<p> Dear ${bname}, <br>
         <br>
         I hope this email does not find you well. In all honesty, my time working for you has been ${aadj} to say the least. I literally cannot find a single fuck to give any longer, and will be pleased to leave my position at ${cname}. Over the past six years, I have never been more ${hadj} to know that I will no longer have to listen to your incessant whining while you sit around making three times my salary.  I will finally be able to pursue my dream of becoming a ${djob}, a dream driven by my burning desire to get away from you. <br>
         <br>
@@ -26,13 +26,15 @@
         ${fname} </p>`;
         
         madl.innerHTML = text;
-
-    });
-
-    document.querySelector('.open').addEventListener('click', function(event){
-        event.preventDefault();
+        console.log(text)
         document.getElementById('overlay').className = 'showing';
+
     });
+
+    // document.querySelector('.open').addEventListener('click', function(event){
+    //     event.preventDefault();
+    //     document.getElementById('overlay').className = 'showing';
+    // });
 
     document.addEventListener('keydown', function(event){
         if (event.key === 'Escape') {
